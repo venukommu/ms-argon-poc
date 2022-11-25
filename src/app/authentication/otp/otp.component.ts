f181488e43080ba10f40f75c2f747d81848586df
Pavan Sandeep <pavansadhu96@gmail.com>
2022-11-25 20:25:45 +0530

    otp page UI

--- src/app/authentication/otp/otp.component.ts
+++ src/app/authentication/otp/otp.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from "src/app/services/auth.service";
 import { Router } from "@angular/router";
 import { NotificationsService } from "src/app/services/notifications.service";
 import { ToolConstService } from "src/app/services/tool-const.service";
+import { FormGroup } from '@angular/forms';
 
 export interface Otp {
   otp: string;
@@ -15,10 +16,14 @@ export interface Otp {
 })
 export class OtpComponent implements OnInit {
 
+  public tokenForm: FormGroup;
+
+
   public loginData: Otp = {
     otp: "",
   };
   inputData: any;
+  otp: any;
 
   constructor(private authService: AuthService, private router: Router,
     private notificationService: NotificationsService,
