f4175651cf04167a39c9291815c456009992f9d4
KasaSandhya <svanapalli@baylogictech.com>
2022-12-08 19:18:31 +0530

    Changed doctor activities functionality with dynamic data

--- src/app/doctoractivities/doctoractivities.component.ts
+++ src/app/doctoractivities/doctoractivities.component.ts
@@ -9,8 +9,31 @@ import { NotificationsService } from "src/app/services/notifications.service";
   styleUrls: ["./doctoractivities.component.scss"],
 })
 export class DoctoractivitiesComponent implements OnInit {
+  public specializations: any;
+  public diagnosis: any;
+  public symptoms:any;
+  public docSpecial: any = [];
+  public userId: any;
+
   constructor(private router: Router, private commonService:CommonService,
-    private notificationService:NotificationsService) {}
+    private notificationService:NotificationsService) {
+      var currentUser = JSON.parse(
+        JSON.stringify(localStorage.getItem("currentUser"))
+      );
+      this.userId = JSON.parse(currentUser)['userId'];
+
+      this.commonService.getSpecialties().subscribe((response) => {
+        this.specializations = response['specializations'];
+      });
+      this.commonService.getSymptoms().subscribe((response) => {
+        this.symptoms = response['symptoms'];
+        console.log("this.symptoms",this.symptoms);
+      });
+      this.commonService.getDiagnosis().subscribe((response) => {
+        this.diagnosis = response['diagnosis'];
+        console.log("this.diagnosis",this.diagnosis);
+      });
+    }
 
   ngOnInit(): void {
     let popup = document.getElementById("popup");
@@ -23,33 +46,43 @@ export class DoctoractivitiesComponent implements OnInit {
   }
 
   openPopup() {
-    let popup = document.getElementById("popup");
-    popup.classList.add("notNone");
-    const body = {
-      id: 1,
-      userId: 15,
-      specializationType: "Physician",
-      specializationTypeId: 1
+    if (this.docSpecial.length > 0) {   
+      this.commonService.doctorActivity(this.docSpecial).subscribe((response) => {
+        if (response['status'] === "true") {
+          this.notificationService.showNotification(
+            response['status'],
+            "success"
+          );
+        } else {
+          this.notificationService.showNotification(
+            response['status'],
+            "danger"
+          );
+        }
+      });
+      let popup = document.getElementById("popup");
+      popup.classList.add("notNone");
+    } else {
+      this.notificationService.showNotification(
+        "Please choose specialization/Symptoms/Diagnosis",
+        "danger"
+      );
     }
-    this.commonService.doctorActivity(body).subscribe((response) => {
-      if (response['status'] === "true") {
-        this.notificationService.showNotification(
-          response['status'],
-          "success"
-        );
-      } else {
-        this.notificationService.showNotification(
-          response['status'],
-          "danger"
-        );
-      }
-    });
-
   }
 
   closePopup() {
     let popup = document.getElementById("popup");
     popup.classList.remove("notNone");
     this.router.navigateByUrl("/landing");
+  }
+
+  selectedItem(id, type, isChecked){
+    if (isChecked) {
+      this.docSpecial.push({'userId': this.userId, 'specializationType': type, 'specializationTypeId': id})
+    } else {
+      if (this.docSpecial.find(x => x.specializationType == type && x.specializationTypeId == id)) {
+        this.docSpecial.splice(this.docSpecial.findIndex(x => x.specializationType == type && x.specializationTypeId == id), 1);
+      }
+    }
   }
 }
